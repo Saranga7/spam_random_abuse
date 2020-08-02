29eb5fa015f815cda2ea5f20cb1b154f55df5537
Saranga7 <53959645+Saranga7@users.noreply.github.com>
2020-08-02 21:31:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
 import re
+import nltk
+nltk.download()
 from nltk.corpus import words
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
